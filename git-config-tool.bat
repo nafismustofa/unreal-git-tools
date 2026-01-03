e1156e72da135439cbd3f3a3a9ce6e49c48ee7f3
nafismustofa <nafismustofa13@gmail.com>
2026-01-04 03:11:50 +0600

    tool updated to auto git init and git lfs install

--- git-config-tool.bat
+++ git-config-tool.bat
@@ -1,9 +1,29 @@
 @echo off
-echo downloading git config files...
 
+if not exist ".git" (
+    echo Initializing Git repository...
+    git init >nul 2>&1
+    echo Git repository initialized.
+) else (
+    echo Git repository already initialized.
+)
+
+echo.
+
+echo Setting up Git LFS in repository...
+
+git lfs install >nul 2>&1
+
+echo Git LFS installed.
+
+echo.
+
+echo Downloading Git config files...
 curl -sS -o .gitignore https://raw.githubusercontent.com/nafismustofa/unreal-git-tools/refs/heads/main/git-config-files/.gitignore
-
 curl -sS -o .gitattributes https://raw.githubusercontent.com/nafismustofa/unreal-git-tools/refs/heads/main/git-config-files/.gitattributes
 
-echo git config files downloaded successfully
+echo Git config files downloaded
+
+echo.
+
 pause
